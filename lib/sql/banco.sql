4c8d7033248f696221cb46eb0e65f389ccb884ea
KastroWalker <victorcsa2002@gmail.com>
2019-11-19 09:05:49 -0300

    Criando banco

---
+++ lib/sql/banco.sql
@@ -0,0 +1,54 @@
+create database Imoveis;
+
+use Imoveis;
+
+create table vendedor(
+    vendedor_id int not null auto_increment,
+    -- Informações Pessoais --
+    nome varchar(200) not null,
+    contato varchar(11),
+    -- Informações da Conta --
+    email varchar(100) not null,
+    user varchar(200) not null,
+    senha varchar(32) not null,
+    primary key(vendedor_id)
+);
+
+create table cliente(
+    cliente_id int not null auto_increment,
+    -- Informações Pessoais --
+    nome varchar(200) not null,
+    contato varchar(11) not null,
+    cpf varchar(11) not null,
+    email varchar(100) not null,
+    -- Informações da Conta --
+    user varchar(200),
+    senha varchar(32),
+    primary key(cliente_id)
+);
+
+create table imoveis(
+    imovel_id int not null auto_increment,
+    endereco varchar(200) not null,
+    numero varchar(200) not null,
+    bairro varchar(200) not null,
+    cep varchar(8) not null,
+    valor_imovel int not null,
+    vendedor_id int not null,
+    primary key(imovel_id),
+    foreign key(vendedor_id) references vendedor(vendedor_id)
+);
+
+create table aluguel(
+    aluguel_id int not null auto_increment,
+    data_inicial date not null,
+    data_final date not null,
+    valor_aluguel int not null,
+    vendedor_id int not null,
+    cliente_id int not null,
+    imovel_id int not null,
+    primary key(aluguel_id),
+    foreign key(vendedor_id) references vendedor(vendedor_id),
+    foreign key(cliente_id) references cliente(cliente_id),
+    foreign key(imovel_id) references imoveis(imovel_id)
+);
