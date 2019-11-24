d05e49f48212730ba1f978168e649f643f1eedc6
kauanphbbb <kauanphbbb@gmail.com>
2019-11-24 14:13:43 -0300

    Mostrar erros de statemente

--- Banco/Conexao.php
+++ Banco/Conexao.php
@@ -1,11 +1,12 @@
 <?php
     class Conexao{
         private $con;
+        private $opcoes = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
         function __construct(){
             try {
-                $this->con = new PDO("mysql:hostname=localhost; dbname=Imoveis", "root", "");
+                $this->con = new PDO("mysql:hostname=localhost; dbname=Imoveis", "root", "", $this->opcoes);
             } catch (PDOException $e) {
-                echo "Erro: ". $e;
+                echo "Não foi possível conectar ". $e;
             };
         }
 
