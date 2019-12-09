f744fee4fd5387ce1a9c2389c1d0d7d8316cd0ab
KastroWalker <victorcsa2002@gmail.com>
2019-12-08 22:39:39 -0300

    Criando a listagem detalhada do cliente

--- View/vendedor/clientes/clientes.php
+++ View/vendedor/clientes/clientes.php
@@ -23,7 +23,18 @@
 
     <link href="../../../lib/css/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="../../../lib/css/simple-sidebar.css" rel="stylesheet">
+    <style>
+        .info_cliente{
+            width: 80%;
+            margin: auto;
+            display: none;
+        }
 
+        #id_cliente{
+            width: 100px;
+            height: 100px;
+        }
+    </style>
 </head>
 
 <body>
@@ -47,9 +58,9 @@
                         <tr>
                             <th>Indice</th>
                             <th>Nome</th>
-                            <th>Contato</th>
+                            <!--th>Contato</th-->
                             <th>CPF</th>
-                            <th>Email</th>
+                            <!--th>Email</th-->
                             <th colspan="2">Ações</th>
                         </tr>
                         <?php
@@ -57,11 +68,14 @@
                         foreach ($dados as $d) {
                             echo "<tr>";
                             echo "<td>".$x."</td>";
-                            #echo "<td>".$d['cliente_id']."</td>";
-                            echo "<td>".$d['nome']."</td>";
-                            echo "<td>".$d['contato']."</td>";
+                            echo "<td><a href='#info_cliente' class='cliente'>".$d['nome']."</a></td>";
                             echo "<td>".$d['cpf']."</td>";
-                            echo "<td>".$d['email']."</td>";
+                            echo "<td style='display: none;'>".$d['cliente_id']."</td>";
+                            echo "<td style='display: none;'>".$d['contato']."</td>";
+                            echo "<td style='display: none;'>".$d['email']."</td>";
+                            echo "<td style='display: none;'>".$d['user']."</td>";
+                            echo "<td style='display: none;'>".$d['senha']."</td>";
+                            echo "<td style='display: none;'>".$d['img_perfil']."</td>";
                             echo "<td><button class='btn btn-info'><img src='../../../img/icon_crud/icon-edit.png'></img></button></td>";
                             echo "<td><button class='btn btn-danger'><img src='../../../img/icon_crud/icon-delete.png'></img></button></td>";
                             echo "</tr>";
@@ -69,11 +83,60 @@
                         }
                         ?>
                     </table>
+                    <div id="info_cliente" class="info_cliente">
+                        <table class='table table-bordered table-hover'>
+                            <tr>
+                                <td>
+                                    <h3>Infomações Cliente</h3>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="" alt="perfil_cliente" id="perfil_cliente">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <dt class='col-sm-3'>Nome: </dt>
+                                    <dd class='col-sm-9' id="nome_cliente"></dd>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <dt class='col-sm-3'>Contato: </dt>
+                                    <dd class='col-sm-9' id="contato_cliente"></dd>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <dt class='col-sm-3'>Cpf: </dt>
+                                    <dd class='col-sm-9' id="cpf_cliente"></dd>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <dt class='col-sm-3'>Email: </dt>
+                                    <dd class='col-sm-9' id="email_cliente"></dd>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <dt class='col-sm-3'>Usuário: </dt>
+                                    <dd class='col-sm-9' id="user_cliente"></dd>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <dt class='col-sm-3'>Senha </dt>
+                                    <dd class='col-sm-9' id="senha_cliente"></dd>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
     <?php 
         escreve_rodape();
     ?>
@@ -81,6 +144,27 @@
         $("#menu-toggle").click(function(e) {
             e.preventDefault();
             $("#wrapper").toggleClass("toggled");
+        });
+        $(document).ready(function(){
+            $('.cliente').on('click', function(){     
+                $("#info_cliente").fadeIn();     
+                
+                $tr = $(this).closest('tr');
+
+                var data = $tr.children("td").map(function(){
+                    return $(this).text();
+                }).get();
+
+                console.log(data);
+                
+                document.getElementById("nome_cliente").innerHTML = data[1];
+                document.getElementById("contato_cliente").innerHTML = data[4];
+                document.getElementById("cpf_cliente").innerHTML = data[2];
+                document.getElementById("email_cliente").innerHTML = data[5];
+                document.getElementById("user_cliente").innerHTML = data[6];
+                document.getElementById("senha_cliente").innerHTML = data[7];
+                document.getElementById("perfil_cliente").src = "../../../img/perfil_clientes/" + data[8];
+            });
         });
     </script>
 </body>
